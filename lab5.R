388c97e68320b134432bb567c9a768b37c2d297a
Dinithi Sanjana <IT22074072@my.sliit.lk>
2024-04-27 15:22:33 +0530

    frequency destribution table

--- lab5.R
+++ lab5.R
@@ -18,4 +18,26 @@ abline(h=0)
 
 #Q2 
 #length = noofclasses + 1
-hist(x2, main = "Histogram for No of Shareholders", breaks = seq(130,270, length = 8), right =FALSE)
+histogram <- hist(x2, main = "Histogram for No of Shareholders", breaks = seq(130,270, length = 8), right =FALSE)
+
+
+#Q3
+#1. identify the break points
+breaks <- round(histogram$breaks)
+breaks
+#2. identify frequencies of each class
+freq <- histogram$counts
+freq
+#3. identify mid points of each class
+mid <- histogram$mids
+mid
+
+classes <- c()
+for(i in 1:length(breaks)-1){
+  classes[i] <- paste0("[", breaks[i], ",", breaks[i+1],"]")
+}
+
+#paste0-concatenate without spaces
+#paste-concatenate with zeros
+
+cbind(Classes=classes, Frequency=freq)
